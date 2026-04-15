43b878aa4c5aaeaa1e48be54a460f726b4a06e1c
annab937 <253284@vut.cz>
2026-04-15 09:38:02 +0200

    def

--- searching.py
+++ searching.py
@@ -2,7 +2,37 @@ from pathlib import Path
 import json
 
 
+
 def read_data(file_name, field):
+    cwd_path = Path.cwd()
+    file_path = cwd_path / file_name
+
+    with open(file_path, mode='r') as file:
+        data = json.load(file)
+
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
+
+
+def linear_search(searched_data, searched_number):
+    idx = 0
+    count = 0
+    positions = []
+    searched_dictionary = {}
+    while idx < len(searched_data):
+        if searched_data[idx] == searched_number:
+            count += 1
+            positions.append(idx)
+        idx += 1
+    searched_dictionary['positions'] = positions
+    searched_dictionary['count'] = count
+    return searched_dictionary
+    
+
+
+    return searched_dictionary
     """
     Reads a JSON file and returns data for a given field.
 
@@ -21,10 +51,15 @@ def read_data(file_name, field):
     cwd_path = Path.cwd()
     
     file_path = cwd_path / file_name
-
+    with open(file_path, mode='r') as file:
+        data = json.load(file)
 
 def main():
-    pass
+    my_data = read_data("sequential.json", "unordered_numbers")
+    print(my_data)
+    found_number = linear_search(my_data, 37)
+    print(found_number)
+
 
 
 if __name__ == "__main__":
