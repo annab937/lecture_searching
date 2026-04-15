63aab8a45a7e44323f685f6e8cfb865008f3c701
annab937 <253284@vut.cz>
2026-04-15 10:45:34 +0200

    def testcomplex

--- searching.py
+++ searching.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 import json
+import time
+import matplotlib.pylot as plt
+
+from generators import unordered_sequence
+
 
 def read_data(file_name, field):
     cwd_path = Path.cwd()
@@ -39,6 +44,26 @@ def binary_search(searched_data, searched_number):
         elif searched_data[mid] > searched_number:
             right = mid - 1
     return None
+
+
+def test_complexity(list_of_n):
+    ordered_data = ordered_sequence(n)
+    duration_linear = 0
+    repetitions = 100
+    for measurements in range(repetitions):
+        start_time_linear = time.perf_counter()
+        found_number = linear_search(unordered_data, number)
+        end_time_linear = time.perf_counter()
+        duration_linear += end_time_linear - start_time_linear
+    for n in list_of_n:
+        unordered_data = unordered_sequence(n)
+        ordered_data = ordered_sequence(n)
+        found_number = linear_search(unordered_data, 42)
+        found_number = binary_search(ordered_data,)
+
+
+def pattern_search():
+
 
 
 def main():
